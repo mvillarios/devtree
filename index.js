48b7ca0e52b842e061ff6143d4b725e9756dfd1d
Miguel Villa Ríos <mvilla2019@inf.udec.cl>
2026-01-24 19:22:54 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+console.log("This is the index.js file.");
+console.log("Welcome to the project!");
+console.log("Let's get started with coding!");
+
+console.log("Enjoy your coding journey!");
+console.log("Happy coding!");
+console.log("Goodbye!");
