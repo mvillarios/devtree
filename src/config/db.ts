b80351a2d4342702af8895d3d4acc3f9817705c2
Miguel Villa Ríos <mvilla2019@inf.udec.cl>
2026-01-25 18:34:09 -0300

    Sección 7 y 8: Registro y Autenticacion de Usuarios

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import User from "../models/User";
 
 export const connectDB = async () => {
   try {
-    const url = process.env.MONGO_URI || "";
-    const connection = await mongoose.connect(url);
-    console.log(colors.magenta.bold("MongoDB connectado"));
+    const { connection } = await mongoose.connect(process.env.MONGO_URI || "");
+    const url = `${connection.host}:${connection.port}`;
+    console.log(
+      colors.magenta.bold("MongoDB conectado en la URL:"),
+      colors.cyan.underline(url),
+    );
   } catch (error) {
     console.error(
       colors.bgRed.white.bold("Error al conectar a MongoDB:"),
